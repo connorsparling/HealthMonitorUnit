d4c90eefa0280708ac75b01a920c6d4a5edf7c19
Connor Sparling <connor.sparling@gmail.com>
2019-11-25 16:05:04 -0500

    Allow configuration to split heartbeat data with QRS in center. Use annotation data for peak location and labeling

--- preprocessData/MIT-BIH_preprocess.py
+++ preprocessData/MIT-BIH_preprocess.py
@@ -28,9 +28,38 @@ def load_data_from_csv(filename, lead_placement):
         return np.array(data)
     return None
 
+def load_data_from_txt(filename):
+    peaks = []
+    heartbeat_types = []
+    if filename is not None:
+        with open(filename) as txt_file:
+            for i, line in enumerate(txt_file):
+                data = line.split(" ")
+                if i > 1:
+                    peak = None
+                    heartbeat_type = None
+                    j = 0
+                    for d in data:
+                        if d != "":
+                            j += 1
+                            if j == 2:
+                                peak = int(d)
+                            elif j == 3:
+                                heartbeat_type = d
+                                break
+                    peaks.append(peak)
+                    heartbeat_types.append(heartbeat_type)
+        txt_file.close()
+    
+    if len(peaks) > 0:
+        return peaks, heartbeat_types
+    return None, None
+
 def load_data(directory, lead_placement):
     data = []
     labels = []
+    peaks = []
+    heartbeat_types = []
     if os.path.exists(directory):
         for filename in os.listdir(directory)[:6]:
             if filename.endswith(".csv"):
@@ -38,11 +67,16 @@ def load_data(directory, lead_placement):
                 d = load_data_from_csv(os.path.join(directory, filename), lead_placement)
                 if d is not None:
                     print("Label {}: {}".format(label, d.shape))
-                    data.append(d)
-                    labels.append(label)
-                else:
-                    print("Label {} does not contain \"{}\"".format(label, lead_placement))
-    return labels, np.array(data)
+                    anno_file = os.path.join(directory, str(label) + "annotations.txt")
+                    p, ht = load_data_from_txt(anno_file)
+                    if p is not None:
+                        data.append(d)
+                        labels.append(label)
+                        peaks.append(p)
+                        heartbeat_types.append(ht)
+                        continue
+                print("Label {} does not contain \"{}\"".format(label, lead_placement))
+    return labels, np.array(data), peaks, heartbeat_types
 
 def normalize_data(data):
     for i in range(data.shape[0]):
@@ -65,7 +99,7 @@ def get_peaks(data):
 def normalize_X(section, desired):
     return sig.resample(section, desired)
 
-def split_peaks(data, peaks, length):
+def split_peak_to_peak(data, peaks, length):
     sections = []
     for i in range(data.shape[0]):
         last_peak = peaks[i][0]
@@ -76,6 +110,27 @@ def split_peaks(data, peaks, length):
             last_peak = p
         sections.append(np.array(local_sections))
     return np.array(sections)
+
+def split_center_peak(data, peaks, heartbeat_types, length):
+    sections = []
+    types = []
+    for i in range(data.shape[0]):
+        last_middle = int(peaks[i][0] + (peaks[i][1] - peaks[i][0])/2)
+        last_peak = peaks[i][1]
+        local_sections = []
+        local_types = []
+        index = 1
+        for p in peaks[i][2:]:
+            middle = int(last_peak + (p - last_peak)/2)
+            section = data[i][last_middle:middle]
+            local_sections.append(normalize_X(section, length))
+            local_types.append(heartbeat_types[i][index])
+            index += 1
+            last_peak = p
+            last_middle = middle
+        sections.append(np.array(local_sections))
+        types.append(local_types)
+    return np.array(sections), types
 
 class SectionPlot():
     def next(self, event):
@@ -93,7 +148,10 @@ class SectionPlot():
 
     def display_plot(self):
         self.fig, self.plot = plt.subplots(figsize=(5, 4))
-        self.fig.canvas.set_window_title("{}: {}".format(self.name, str(self.index)))
+        detail_name = str(self.index)
+        if self.types is not None:
+            detail_name = self.types[self.index]
+        self.fig.canvas.set_window_title("{}: {}".format(self.name, detail_name))
         plt.subplots_adjust(bottom=0.2)
 
         plt.plot(self.sections[self.index])
@@ -112,9 +170,10 @@ class SectionPlot():
 
         plt.show()
 
-    def __init__(self, name, sections):
+    def __init__(self, name, sections, types=None):
         self.name = name
         self.sections = sections
+        self.types = types
         self.index = 0
         self.display_plot()
 
@@ -175,10 +234,11 @@ def main(argv):
     filename = None
     lead_placement = "MLII"
     plot_type = None
-    downsample_amount = 1
-    function_format = "MITBIH_preprocess.py [-f <input_file>] [-l <lead_placement>] [-d <downsample_amount>] [-p <plot_type>]"
+    downsample_amount = None
+    split_location = None
+    function_format = "MITBIH_preprocess.py [-f <input_file>] [-l <lead_placement>] [-d <downsample_amount>] [-p <plot_type>] [-s <split_location>]"
     try:
-        opts, args = getopt.getopt(argv,"hd:f:l:p:",["file=", "lead=", "plot=", "downsample="])
+        opts, args = getopt.getopt(argv,"hd:f:l:p:s:",["file=", "lead=", "plot=", "downsample=", "split="])
     except getopt.GetoptError:
         print("INCORRECT FORMAT: \"" + function_format + "\"")
         sys.exit(2)
@@ -190,27 +250,44 @@ def main(argv):
             filename = arg
         elif opt in ("-l", "--lead"):
             lead_placement = arg
-        elif opt in ("-d", "--downsample"):
-            downsample_amount = int(arg)
+        # elif opt in ("-d", "--downsample"):
+        #     downsample_amount = int(arg)
         elif opt in ("-p", "--plot"):
             plot_type = arg
+        elif opt in ("-s", "--split"):
+            split_location = arg
 
     if lead_placement is None:
         print("Please input a lead placement position")
         sys.exit()
 
-    labels, data = load_data(filename, lead_placement)
+    labels, data, peaks, heartbeat_types = load_data(filename, lead_placement)
     data = normalize_data(data)
-    data = downsample(data, downsample_amount)
-    peaks = get_peaks(data)
-    sections = split_peaks(data, peaks, 200)
+
+    # if downsample_amount is not None:
+    #     data = downsample(data, downsample_amount)
+
+    #peaks = get_peaks(data) # OLD METHOD
+
+    sections = None
+    if split_location == "peak":
+        sections = split_peak_to_peak(data, peaks, 200)
+        heartbeat_types = None #******** CAREFULL
+    elif split_location == "center":
+        sections, heartbeat_types = split_center_peak(data, peaks, heartbeat_types, 200)
 
     if plot_type == "range":
         for i in range(data.shape[0]):
             RangePlot("Label {}".format(labels[i]), data[i], peaks[i], length=2000)
     if plot_type == "section":
+        if sections is None:
+            print("Please select a split location ('peak' or 'center')")
         for i in range(sections.shape[0]):
-            SectionPlot("Label {}".format(labels[i]), sections[i])
+            SectionPlot("Label {}".format(labels[i]), sections[i], types=heartbeat_types[i])
+
+# Example Command:
+#   python3 preprocessData/MIT-BIH_preprocess.py -f Datasets/mitbih-database -l MLII -p section -s center
+#   python3 preprocessData/MIT-BIH_preprocess.py -f Datasets/mitbih-database -l MLII -p range
             
         
 if __name__ == '__main__':
